441927fa2a8dbc0447c77bfa5d7928c90d240ec2
francksefu <francksefu1998@gmail.com>
2025-05-17 09:38:54 +0200

    Initial commit

---
+++ app/categories/page.tsx
@@ -0,0 +1,9 @@
+const CategoriePage = async () => {
+    return (
+        <main>
+            <h1 className="text-2xl">Categories of expenses</h1>
+        </main>
+    );
+}
+
+export default CategoriePage;
